0ea8b97f6c2231ff93b6d74f2b908630fe3fab81
Syed Taha Hassan <stahay2k@gmail.com>
2020-09-12 16:01:40 -0700

    Timer Commit 1

---
+++ Hellscape/Assets/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float remainingTime = 15f;
+    public bool timeFlag = false;
+    
+    public void Start()
+    {
+        timeFlag = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeFlag == true){
+            if (remainingTime > 0f){
+                remainingTime -= Time.deltaTime;
+            }else{
+                resetTimer();
+            }
+
+        }
+    }
+    void resetTimer(){
+        remainingTime = 0f;
+        timeFlag = false;
+        Debug.Log("You are Out Of Time!!!");
+    }
+}
