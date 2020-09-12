889d7dcb3a486f353c2ec5d26ec38efc19da90ca
varmagokul6 <varmagokul6@gmail.com>
2020-09-12 14:33:06 -0700

    Added Health Bar

---
+++ Hellscape/Assets/HealthBar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    
+    public Slider slider;
+	
+	public void SetHealth(int health){
+		slider.value = health;
+	}
+}
