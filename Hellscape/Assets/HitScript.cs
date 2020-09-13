61f6b0b7e02c9f00a97cc0a2385c26ba5ac8e79d
k0ji13 <thekoji007@gmail.com>
2020-09-12 18:57:04 -0700

    Artwork More

---
+++ Hellscape/Assets/HitScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject hitPic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Monster"))
+        {
+            Instantiate(hitPic, new Vector2(transform.position.x + 1f, transform.position.y), Quaternion.identity);
+        }
+    }
+}
