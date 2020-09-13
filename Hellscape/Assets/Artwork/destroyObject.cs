8d1b2b5016b7af5f80d7b9f57269f83d655f9ffd
k0ji13 <thekoji007@gmail.com>
2020-09-12 19:20:43 -0700

    New Animations

---
+++ Hellscape/Assets/Artwork/destroyObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 0.2f);
+    }
+    
+}
--- Hellscape/Assets/HitScript.cs
+++ Hellscape/Assets/HitScript.cs
@@ -6,17 +6,7 @@ public class HitScript : MonoBehaviour
 {
     [SerializeField]
     GameObject hitPic;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
