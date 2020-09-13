5bcc2a497485afc03fae240bfc5d1cd3e4c80312
Gokul Varma <44277090+varmagokul6@users.noreply.github.com>
2020-09-12 20:55:16 -0700

    Add files via upload

---
+++ HealthBar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    
+    public Slider slider;
+	
+	public void SetHealth(float health){
+		Debug.Log("Slider: " + health.ToString());
+		slider.value = 1000 * health/50000;
+	}
+}
